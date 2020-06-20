215781a300ec9e97db46ba00014e44e994f7cf81
David Cruwys <david.cruwys@bugcrowd.com>
2020-06-20 11:50:49 +1000

    config settings and view

---
+++ lib/poc_shotstack/commands/config_settings.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../command'
+
+require 'tty-config'
+require 'tty-prompt'
+
+module PocShotstack
+  module Commands
+    # Set all settings
+    class ConfigSettings < PocShotstack::Command
+      def initialize(options)
+        super(options)
+      end
+
+      # Execute ConfigSettings subcommand taking input from
+      # a input stream and writing to output stream
+      #
+      # sample: output.puts 'OK'
+      def execute(input: $stdin, output: $stdout)
+        :gui
+      end
+    end
+  end
+end
