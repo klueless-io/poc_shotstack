2d6817da5b45481fd51ffc74a5ffc6854ed4d9b2
David Cruwys <david.cruwys@bugcrowd.com>
2020-06-20 10:05:45 +1000

    Initial Commit

---
+++ lib/poc_shotstack.rb
@@ -0,0 +1,6 @@
+require "poc_shotstack/version"
+
+module PocShotstack
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/poc_shotstack/version.rb
@@ -0,0 +1,3 @@
+module PocShotstack
+  VERSION = "0.1.0"
+end
